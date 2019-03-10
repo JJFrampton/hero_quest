65b9f72211ad4610a27d47a1bcf21dcd24ef64bf
joseph frampton <josephjamesframpton@gmail.com>
2019-03-10 13:34:09 -0600

    pushing board printing options
    JJF

--- test_board.py
+++ test_board.py
@@ -10,4 +10,28 @@ board = board.replace('t',',')
 board = board.replace('b','\'')
 board = board.replace('j','+')
 board = board.replace('o',' ')
+print()
+print("PANDAS BOARD")
 print(board)
+print("NOTES:")
+print("might be too spaced out, difficult to tell where gaps are")
+
+board = board.to_numpy()
+
+print()
+print("NORMAL BOARD")
+for y in range(len(board)):
+    for x in range(len(board[y])):
+        print(board[y][x], end='')
+    print('')
+print("NOTES:")
+print("best option, programatically")
+
+print()
+print("HORIZONTALLY SPACED BOARD")
+for y in range(len(board)):
+    for x in range(len(board[y])):
+        print("%s " % board[y][x], end='')
+    print('')
+print("NOTES:")
+print("no issues with counting spaces as this is just for printing")
