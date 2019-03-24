196b37a3aa39efc25650c837d7cd97f47d8e6835
Joseph Frampton <josephjamesframpton@gmail.com>
2019-03-23 23:14:53 -0600

    cleaning up files, lowercased all dirs and files, fixed movment bug
    - movement / collision checking not throughly tested
    - movement / collision checking updates new player location but does not remove the old marker
    
    JJF

--- Characters/Enemies/Enemy.py
+++
@@ -1,4 +0,0 @@
-from Characters.Character import Character
-class Enemy(Character):
-    def movement_roll(self):
-        self.movement = self.stats_move
--- Characters/Players/Player.py
+++
@@ -1,5 +0,0 @@
-from Characters.Character import Character
-class Player(Character):
-    def movement_roll(self):
-        Character.moved = True
-        self.movement = Character.d.roll('r', self.stats_move)
